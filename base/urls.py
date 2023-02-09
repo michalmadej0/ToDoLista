19dda96facbf9857c7e1e307e3f61eebcdb07ade
Michal <m.madejlodz@gmail.com>
2023-02-09 18:55:53 +0100

    first commit

---
+++ base/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView,\
+    CustomLoginView
+
+urlpatterns = [
+    path('login', CustomLoginView.as_view(), name='login'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-updata/<int:pk>/', TaskUpdate.as_view(), name='task-updata'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
+
+
+]
